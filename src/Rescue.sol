34433b7b5db8eec405b6806e9f7f57e85d1988dd
Lyn <0xlynett@proton.me>
2024-11-04 13:52:20 +0000

    aaa

---
+++ src/Rescue.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.13;
+import {Module} from "@gnosis-guild/zodiac/contracts/core/Module.sol";
+import {Enum} from "@gnosis.pm/safe-contracts/contracts/common/Enum.sol";
+import {ISafe} from "@safe-global/safe-smart-account/contracts/interfaces/ISafe.sol";
+import {IOwnerManager} from "@safe-global/safe-smart-account/contracts/interfaces/IOwnerManager.sol";
+
+/// @title Rescue
+/// @author L3 (for 2077 Collective)
+/// A module that can be called at any time by the owner to set the safe's
+/// signers to the owner in case the existing signers are incapacitated
+/// or unavailable in some way. The owner should be another Safe, but may
+/// not be.
+contract Rescue is Module {
+    constructor(address _owner, address _avatar, address _target) {
+        setUp(abi.encode(_owner, _avatar, _target));
+    }
+
+    function setUp(
+        bytes memory initializeParams
+    ) public virtual override initializer {
+        (address _owner, address _avatar, address _target) = abi.decode(
+            initializeParams,
+            (address, address, address)
+        );
+        __Ownable_init(_owner);
+        require(_avatar != address(0), "Avatar can not be zero address");
+        setAvatar(_avatar);
+        setTarget(_target);
+    }
+
+    function rescue() public onlyOwner {
+        ISafe safe = ISafe(avatar);
+        address[] memory owners = safe.getOwners();
+
+        // Thanks for the code!
+        // https://github.com/5afe/CandideWalletContracts/blob/113d3c059e039e332637e8f686d9cbd505f1e738/contracts/modules/social_recovery/SocialRecoveryModule.sol
+        for (uint256 i = (owners.length - 1); i > 0; --i) {
+            bool success = exec({
+                to: avatar,
+                value: 0,
+                data: abi.encodeCall(
+                    IOwnerManager.removeOwner,
+                    (owners[i - 1], owners[i], 1)
+                ),
+                operation: Enum.Operation.Call
+            });
+            require(success, "Rescue: Owner removal failed");
+        }
+
+        // Set signers to the owner
+    }
+}
